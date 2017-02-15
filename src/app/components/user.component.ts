31c87e8a47bb3569a95eb0df2f9a199395bb349c
Aaron Teague <aaron.teague@outlook.com>
2017-02-15 11:55:26 -0600

    Finishing up this week's homework

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -52,64 +52,35 @@ import { Component } from '@angular/core';
     </tr>
   </table>
 
-  <div *ngIf = "showHobbies">
-    <h3>Hobbies</h3>
-    <ul>
-      <li *ngFor="let hobby of hobbies; let i = index;">
-        {{hobby}} <button (click)="deleteHobby(i)">X</button>
-      </li>
-    </ul>
-    <form (submit)="addHobby(hobby.value)">
-      <label>Add Hobby: </label> <br/>
-      <input type="text" #hobby /><br/>
-    </form>
-  </div>
-  <hr />
-  <h3>Edit User</h3>
-  <form>
-    <label>Name: </label> <br/>
-    <input type="text" name="name" [(ngModel)]="name" /><br/>
-    <label>Email: </label> <br/>
-    <input type="text" name="email" [(ngModel)]="email" /><br/>
-    <label>Street: </label> <br/>
-    <input type="text" name="address.street" [(ngModel)]="address.street" /><br/>
-    <label>City: </label> <br/>
-    <input type="text" name="address.city" [(ngModel)]="address.city" /><br/>
-    <label>State: </label> <br/>
-    <input type="text" name="address.state" [(ngModel)]="address.state" /><br/>
-  </form>
+
+ 
   `,
 })
 export class UserComponent {
   name: string;
   email: string;
-  address: address;
-  hobbies: string[];
-  showHobbies: boolean;
+
   assignmentList: assignment[];
   assignmentName: string;
   assignmentPointsPossible: number;
   assignementPointsScored: number;
 
   constructor() {
-    this.name = 'Aaron';
-    this.email = "aaron@gmail.com";
-    this.address = {
-      street: '12 main street',
-      city: 'Bentonville',
-      state: 'AR'
-    }
-    let item = {name: "banana", pointsScored: 7, pointsPossible: 13};
-    let item2 = {name: "hammock", pointsScored: 3, pointsPossible: 18};
+    this.name = 'Aaron Teague';
+    this.email = "aaron.teague@outlook.com";
+
+    let item = {name: "Angular Homework", pointsScored: 7, pointsPossible: 13};
+    let item2 = {name: "Ionic Homework", pointsScored: 3, pointsPossible: 18};
     this.assignmentList = [item, item2];
-    this.hobbies = ['Music', 'Movies', 'Sports'];
-    this.showHobbies = false;
+
+
 
     
   }
 
   addAssignment(): void {
    let a = {name: this.assignmentName, pointsScored: this.assignementPointsScored, pointsPossible: this.assignmentPointsPossible}
+   this.assignmentList.push(a);
    this.assignmentName = "";
    this.assignementPointsScored = 0;
    this.assignmentPointsPossible = 0;
@@ -158,10 +129,4 @@ interface assignment {
   name: string,
   pointsScored: number,
   pointsPossible: number
-}
-
-interface address {
-  street: string;
-  city: string;
-  state: string;
 }
